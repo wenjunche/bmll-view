2d9690340748ecd5c8d9722ad9b490aaa005e912
wenjunche <wenjun@openfin.co>
2022-05-04 20:24:52 -0400

    added sort and filter

--- src/components/Isin.tsx
+++ src/components/Isin.tsx
@@ -19,12 +19,13 @@ const xnasData = require('../../data/xnas.json');
 
 const testSecurities: any[] = [];
 const testTrades: any[] = [];
-const XNASList = 'AAPL,ADBE,ADI,ADP,ADSK,AEP,AEP,ALGN,AMAT,AMD,AMGN,AMZN,ANSS,ASML,ATVI,AVGO,BIDU,BIIB,BKNG,CDNS,CDW,CERN,CHKP,CHTR,CMCSA,COST,CPRT,CRWD,CSCO,CSX,CTAS,CTSH,DLTR,DOCU,DXCM,EA,EBAY,EXC,EXC,FAST,FB,FISV,FOX,FOXA,GILD,GOOG,GOOGL,HON,HON,IDXX,ILMN,INCY,INTC,INTU,ISRG,JD,KDP,KDP,KHC,KLAC,LRCX,LULU,MAR,MCHP,MDLZ,MELI,MNST,MRNA,MRVL,MSFT,MTCH,MU,NFLX,NTES,NVDA,NXPI,OKTA,ORLY,PAYX,PCAR,PDD,PEP,PEP,PTON,PYPL,QCOM,REGN,ROST,SBUX,SGEN,SIRI,SNPS,SPLK,SWKS,TCOM,TEAM,TMUS,TSLA,TXN,VRSK,VRSN,VRTX,WBA,WDAY,WDAY,XEL,XEL';
-const XLONList = 'AAL,ABDN,ABF,ADM,AHT,ANTO,AUTO,AV.,AVST,AVV,AZN,BA.,BARC,BATS,BDEV,BHP,BKG,BLND,BME,BNZL,BP.,BRBY,BT.A,CCH,CPG,CRDA,CRH,DCC,DGE,DOCS,ENT,EVR,EXPN,FERG,FLTR,FRES,GLEN,GSK,HIK,HL.,HLMA,HSBA,IAG,ICP,IHG,III,IMB,INF,ITRK,ITV,JD.,JMAT,KGF,LAND,LGEN,LLOY,LSEG,MNDI,MNG,MRO,NG.,NWG,NXT,OCDO,PHNX,POLY,PRU,PSN,PSON,RDSB,REL,RIO,RKT,RMG,RMV,RR.,RTO,SBRY,SDR,SGE,SGRO,SKG,SMDS,SMIN,SN.,SPX,SSE,STAN,STJ,SVT,THG,TSCO,TW.,ULVR,UU.,VOD,WEIR,WPP,WTB';
-const parseTickerList = (list: string, mic: string):FDC3Instrument[] => {
-    return list.split(',').map(value => {
-        return { type: FDC3.ContextType, name: value, id: { ticker: value}, mic };
-    });
+const TOP_XLONList = 'HEL,AZN,LIN,HSBC,UL,DEO,RIO,GSK,BP,BTI';
+const TOP_XNASList = 'AAPL,MSFT,GOOG,AMZN,TSLA,BRK.A,FB,NVDA,JNJ,UNH';
+const selectTickers = (list: string, data): any[] => {
+    const tickers = list.split(',');
+    return data.grid.filter(item => {
+        return list.includes(item.Ticker);
+    })
 }
 
 window.addEventListener("DOMContentLoaded",  async () => {
@@ -35,11 +36,12 @@ window.addEventListener("DOMContentLoaded",  async () => {
     }
 
     testSecurities.length = 0;
-    testSecurities.push(...xlonData.grid.slice(0, 30));
-    testSecurities.push(...xnasData.grid.slice(0, 30));
-    testSecurities.sort((s1, s2) =>{
-        return s1.Ticker.localeCompare(s2.Ticker);
-    });
+    testSecurities.push(...selectTickers(TOP_XLONList, xlonData));
+    testSecurities.push(... selectTickers(TOP_XNASList, xnasData));
+    testTrades.push(...generateFakeTrades(testSecurities));
+    testTrades.push(...generateFakeTrades(testSecurities));
+    testTrades.push(...generateFakeTrades(testSecurities));
+    testTrades.push(...generateFakeTrades(testSecurities));
     testTrades.push(...generateFakeTrades(testSecurities));
 
     const w = await (fin.me as OpenFin.View).getCurrentWindow();
@@ -60,16 +62,16 @@ const launchPartnerApp = async() => {
 export const IsinDropdown: React.FC = () => {
     const isinSelectRef = useRef<HTMLSelectElement>();
     const [columnDefs] = useState<Array<ColDef>>([
-        { field: 'Account', resizable: true, autoHeight: true  },
-        { field: 'OrderID', resizable: true, autoHeight: true  },
-        { field: 'Date', resizable: true, autoHeight: true  },
-        { field: 'Ticker', resizable: true, autoHeight: true  },
-        { field: 'Name', resizable: true, autoHeight: true  },
-        { field: 'Side',  resizable: true, autoHeight: true  },
-        { field: 'OPOL',  resizable: true, autoHeight: true  },
-        { field: 'Quantity',  resizable: true, autoHeight: true  },
-        { field: 'Price',  resizable: true, autoHeight: true  },
-        { field: 'CCY',  resizable: true, autoHeight: true  },
+        { field: 'Account', resizable: true, autoHeight: true, sortable: true },
+        { field: 'OrderID', resizable: true, autoHeight: true, sortable: true  },
+        { field: 'Date', resizable: true, autoHeight: true, sortable: true  },
+        { field: 'Ticker', resizable: true, autoHeight: true, sortable: true, filter: 'agTextColumnFilter'  },
+        { field: 'Name', resizable: true, autoHeight: true, sortable: true, filter: 'agTextColumnFilter'  },
+        { field: 'Side',  resizable: true, autoHeight: true , sortable: true },
+        { field: 'OPOL',  resizable: true, autoHeight: true, sortable: true  },
+        { field: 'Quantity',  resizable: true, autoHeight: true, sortable: true  },
+        { field: 'Price',  resizable: true, autoHeight: true, sortable: true  },
+        { field: 'CCY',  resizable: true, autoHeight: true, sortable: true  },
     ]);
     const [rowData] = useState(testTrades);
 
